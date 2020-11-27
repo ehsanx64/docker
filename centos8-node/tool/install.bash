6a2c585f8b0cc1e6dd6339115b3dfd28f08b5aa8
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-11-27 22:01:18 +0330

    Add centos8-node

---
+++ centos8-node/tool/install.bash
@@ -0,0 +1,11 @@
+echo "### Installing Node.js..."
+echo "# Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+echo '# Node.js v12.16.2 LTS'
+nvm install --lts v12.16.2
+nvm ls
